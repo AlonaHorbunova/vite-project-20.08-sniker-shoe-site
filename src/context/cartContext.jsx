0a31c8129978c8c5a4ce7c2148c19280cd95d8c1
AlonaHorbunova <AlonaHorbunova@proton.me>
2025-08-20 23:55:40 +0200

    main

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,7 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "https://664623b951e227f23aadf146.mockapi.io";
+export const BASE_URL = "https://664623b951e227f23aadf146.mockapi.io";
 
 const CartContext = createContext();
 
@@ -11,15 +11,21 @@ const CartProvider = ({ children }) => {
   async function fetchCartItems() {
     try {
       const response = await axios.get(`${BASE_URL}/cartData`);
+      setCartItems(response.data);
     } catch (error) {
       console.error("Error fetching cart items:", error);
     }
   }
-  // add item to cart // post -> BASE_URL/cartData // remove item from cart // delete -> BASE_URL/cartData/productId
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
   return (
     <CartContext.Provider value={{ cartItems }}>
       {children}
     </CartContext.Provider>
   );
 };
+
 export default CartProvider;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../../context/cartContext";
+import axios from "axios";
+
 function Main() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  async function fetchProducts() {
+    try {
+      const response = await axios.get(`${BASE_URL}/productData`);
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error occured when fetching products: ", error);
+      setError("Не удалось загрузить товары.");
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <h1>Main page</h1>
-    </div>
+    <main>
+      <h1>Товары</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div>
+        {products.map(({ id, name, image, price }) => (
+          <div key={id}>
+            <h1>{name}</h1>
+            <img src={image} alt={name} />
+            <h2>Цена: {price}</h2>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
+
 export default Main;
