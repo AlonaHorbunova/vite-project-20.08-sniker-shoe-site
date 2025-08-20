8859bd23705a8a4271d5937467260abc3fac593f
AlonaHorbunova <AlonaHorbunova@proton.me>
2025-08-20 22:49:54 +0200

    header+routing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Routes, Route } from "react-router";
+import Header from "./components/header";
+import Main from "./pages/main";
+import Cart from "./pages/cart";
+import Contacts from "./pages/contacts";
+import CartProvider from "./context/cartContext";
+
+function App() {
+  return (
+    <CartProvider>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="*" element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </div>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/contacts/index.jsx
@@ -0,0 +1,8 @@
+function Contacts() {
+  return (
+    <div>
+      <h1>Contacts page</h1>
+    </div>
+  );
+}
+export default Contacts;
